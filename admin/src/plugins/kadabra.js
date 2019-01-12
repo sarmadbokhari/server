47c3482fe349e5cbba30e448cc030aaf00dba379
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2019-01-11 21:23:59 -0800

    Update url to kadabraServerUrl

--- admin/src/plugins/kadabra.js
+++ admin/src/plugins/kadabra.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import kadabraClient from '@kadabra/client'
 
-let url = window.location.href
+let kadabraServerUrl = window.location.href
 try {
   if (webpackHotUpdate) {
-    url = 'localhost:7777'
+    kadabraServerUrl = 'localhost:7777'
   }
 } catch(_) {}
 
-const service = kadabraClient(url)
+const service = kadabraClient(kadabraServerUrl)
 export const feathersClient = service('users').client
 
 Vue.prototype.$K = service
